8d700a2922f5d07440ad93259499ab0338158a51
Grzegorz 'Uirapuru' Kaszuba <uirapuruadg@gmail.com>
2015-09-03 18:07:17 +0200

    mend

--- TPNEmailTemplatesBundle.php
+++ TPNEmailTemplatesBundle.php
@@ -2,10 +2,12 @@
 namespace TPN\EmailTemplatesBundle;
 
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TPNEmailTemplatesBundle extends Bundle {
     public function build(ContainerBuilder $container)
     {
+
     }
 }
